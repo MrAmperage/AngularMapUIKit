bb0c0b4732b3a647f7ac836f673b5199abe39cee
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-09-13 18:09:17 +0700

    Добавлена иконка бурового станка

---
+++ src/SVGIconComponents/DrillingRigSVGIconComponent/DrillingRigSVGIconComponent.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+import BaseSVGIconDirective from "../BaseSVGIconDirective/BaseSVGIconDirective";
+/*Компонент иконки бурового станка */
+@Component({
+  selector: "DrillingRigSVGIconComponent",
+  templateUrl: "DrillingRigSVGIconComponent.svg",
+})
+export default class DrillingRigSVGIconComponent extends BaseSVGIconDirective {}
--- src/public-api.ts
+++ src/public-api.ts
@@ -1,4 +1,5 @@
+/*Общие компоненты*/
 export { default as DateTimeIntervalComponent } from "./Components/DateTimeIntervalComponent/DateTimeIntervalComponent";
-export { default as DrillingProjectsWidgetSwitchersComponent } from "./Components/DrillingProjectsWidgetSwitchersComponent/DrillingProjectsWidgetSwitchersComponent";
 export { default as TitlePanelComponent } from "./Components/TitlePanelComponent/TitlePanelComponent";
-export { default as TransportsWidgetSwitchersComponent } from "./Components/TransportsWidgetSwitchersComponent/TransportsWidgetSwitchersComponent";
+/*Иконки*/
+export { default as DrillingRigSVGIconComponent } from "./SVGIconComponents/DrillingRigSVGIconComponent/DrillingRigSVGIconComponent";
